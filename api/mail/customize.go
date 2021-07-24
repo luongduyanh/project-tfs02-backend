769a73a814a78ba69a851859e2aaca9476dea3b9
luongduyanh <luongduyanh1999@gmail.com>
2021-07-24 07:10:02 +0700

    .

--- api/mail/customize.go
+++ api/mail/customize.go
@@ -4,6 +4,28 @@ import (
 	"fmt"
 )
 
+//gửi code xác minh khi người dùng chọn quên mật khẩu
+func SendCodeVerify(code int, receiverName, receiverEmail string) {
+	var receiver = EmailUser{
+		Name:  receiverName,
+		Email: receiverEmail,
+	}
+
+	var emailContent = EmailContent{
+		ID:               0,
+		Subject:          "Xác thực tài khoản",
+		FromUser:         &Sender,
+		ToUser:           &receiver,
+		PlainTextContent: "_",
+		HtmlContent:      fmt.Sprintf("Mã xác thực là %v", code),
+	}
+
+	var sengrid = NewSendgrid(ApiKey)
+	//send
+	sengrid.Send(&emailContent)
+	fmt.Println("sent email")
+}
+
 //gửi email thông báo đăng kí thành công
 func SendNoticeRegisterSuccessful(user_name, user_email string) {
 	var receiver = EmailUser{
@@ -12,7 +34,7 @@ func SendNoticeRegisterSuccessful(user_name, user_email string) {
 	}
 	var emailContent = EmailContent{
 		ID:               0,
-		Subject:          "Đăng ký thành công",
+		Subject:          "Đăng nhập thành công",
 		FromUser:         &Sender,
 		ToUser:           &receiver,
 		PlainTextContent: "_",
